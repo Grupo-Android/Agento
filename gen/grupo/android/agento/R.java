518b2174edfb682cf02e29e77a59489761900dc5
Lucas Mendes <lucas.duque@fatec.sp.gov.br>
2012-11-24 09:58:37 -0200

    Eventos Add

--- gen/grupo/android/agento/R.java
+++ gen/grupo/android/agento/R.java
@@ -33,7 +33,8 @@ public final class R {
         public static final int ic_action_search=0x7f020003;
         public static final int ic_error=0x7f020004;
         public static final int ic_launcher=0x7f020005;
-        public static final int plus=0x7f020006;
+        public static final int pen=0x7f020006;
+        public static final int plus=0x7f020007;
     }
     public static final class id {
         public static final int MensagemErroTextView=0x7f0a000f;
@@ -43,13 +44,13 @@ public final class R {
         public static final int button2=0x7f0a0003;
         public static final int button3=0x7f0a0001;
         public static final int cancelarButton=0x7f0a000a;
-        public static final int checkBox1=0x7f0a000c;
         public static final int emailCadastroEditText=0x7f0a0009;
         public static final int menu_settings=0x7f0a0010;
         public static final int scrollView1=0x7f0a000b;
         public static final int senhaCadastroEditText=0x7f0a0008;
         public static final int senhaEditText=0x7f0a0004;
         public static final int spinner1=0x7f0a000e;
+        public static final int tableLayout=0x7f0a000c;
         public static final int textView1=0x7f0a0005;
         public static final int usuarioCadastroEditText=0x7f0a0007;
         public static final int usuarioEditText=0x7f0a0000;
--- src/grupo/android/agento/AgentoEvento.java
+++ src/grupo/android/agento/AgentoEvento.java
@@ -2,14 +2,19 @@ package grupo.android.agento;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
-public class AgentoEvento extends Activity {
-
+public class AgentoEvento extends Activity { 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,27 @@ public class AgentoEvento extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.agento_evento, menu);
         return true;
+    }
+    
+    public void addEvento(View v){
+    	TableLayout tl = (TableLayout)findViewById(R.id.tableLayout);
+        //Criando uma nova linha
+        TableRow tr = new TableRow(this);
+        tr.setWeightSum(1);
+        //Criando os componentes
+        ImageButton button = new ImageButton(this);
+        button.setImageResource(R.drawable.pen);
+        
+        TextView text = new TextView(this);
+        text.setText("Arraste até a lixeira para excluir");
+        
+        CheckBox box = new CheckBox(this);
+        
+        //Adicionando os componentes na linha
+        tr.addView(button);
+        tr.addView(text);
+        tr.addView(box);
+        //Adicionando a linha no layout
+        tl.addView(tr);
     }
 }
